7b4566564ef339d0ce1e1e9b80dbcd6e61ee17ad
juliojurema <zueranever3j@gmail.com>
2025-05-13 17:54:34 -0300

    Mudança na estrutura da Navbar e Estilo

--- src/Components/Navbar/index.tsx
+++ src/Components/Navbar/index.tsx
@@ -1,18 +1,25 @@
 import React from "react";
 import './style.css';
 import Logo from "../../assets/Logo.png"
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+
+interface navProps{
+    isActive: boolean;
+}
 
 
-const Navbar: React.FC = () =>{
+const Navbar: React.FC<navProps> = () =>{
     return(
         <nav className="navbarContainer">
             <img src={Logo} alt="Logo do site" />
             <div className="navArea">
                 <div className="navButtons">
-                    <Link to='/'>Dashboard</Link>
-                    <Link to='/simulado'>Simulado</Link>
-                    <Link to='/livre'>Livre</Link>
+                        <NavLink to='/' className={({ isActive }) => isActive ? 'navItens navActive' : 'navItens'}>Dashboard</NavLink>
+                    <div className="navDiv1">
+                        <NavLink to='/simulado' className={({ isActive }) => isActive ? 'navItens navActive' : 'navItens'}>Simulado</NavLink>
+                        <NavLink to='/livre' className={({ isActive }) => isActive ? 'navItens navActive' : 'navItens'}>Livre</NavLink>
+                    </div>
+                    
                 </div>
                 <div>
                     <p>sair</p>
