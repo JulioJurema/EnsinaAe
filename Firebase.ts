a9c581306b6bf6bfa187bbae811699f7be6725cf
juliojurema <zueranever3j@gmail.com>
2025-06-28 12:05:09 -0300

    Firebase integrado, sistema de login feito!

---
+++ Firebase.ts
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDK61Bl3skSS2He3Va5mws3cJCgt_yHVQw",
+  authDomain: "ensinaae-6bb52.firebaseapp.com",
+  projectId: "ensinaae-6bb52",
+  storageBucket: "ensinaae-6bb52.firebasestorage.app",
+  messagingSenderId: "823042371307",
+  appId: "1:823042371307:web:1fe75f8d1d3730c8f2d8ae",
+  measurementId: "G-PZ2RZKFDFJ"
+}
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+const googleProvider = new GoogleAuthProvider();
+
+export { auth, googleProvider };
--- src/Pages/LoginPage/LoginPage.tsx
+++ src/Pages/LoginPage/LoginPage.tsx
@@ -1,20 +1,38 @@
 import './style.css'
 import Logo from '../../assets/Logo.png'
+import { signInWithPopup } from 'firebase/auth';
+import { auth, googleProvider } from '../../../Firebase';
+import { useNavigate } from 'react-router-dom';
 
+const LoginPage: React.FC = () => {
+  const navigate = useNavigate();
 
-const LoginPage: React.FC = () =>{
-    return(
-        <section className='loginPageContainer'>
-            <div className='loginPageHeader'>
-                <img src={Logo} alt="Logo do site" />
-            </div>
-            <div className='loginPageBody'>
-                <div className='loginCard'>
-                    <p>card</p>
-                </div>
-            </div>
-        </section>
-    )
+  const handleGoogleLogin = async () => {
+    try {
+      await signInWithPopup(auth, googleProvider);
+      navigate('/');
+    } catch (error) {
+      console.error('Erro no login com Google:', error);
+    }
+  };
+
+  return (
+    <section className='loginPageContainer'>
+      <div className='loginPageHeader'>
+        <img src={Logo} alt="Logo do site" />
+      </div>
+      <div className='loginPageBody'>
+        <div className='loginCard'>
+          <div className='loginInfo'>
+            <p>Entre na plataforma</p>
+          </div>
+          <div className='loginButtons'>
+            <button onClick={handleGoogleLogin}>Entrar com Google</button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default LoginPage;
